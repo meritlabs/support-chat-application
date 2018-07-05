96c1798d1734e6ec8364d39066271e3cb9999113
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-05 18:31:47 +0300

    Beautify init message textarea

--- src/chat-form/components/main/main.view.ts
+++ src/chat-form/components/main/main.view.ts
@@ -7,7 +7,7 @@ export default function template() {
     <div class="primaryDescription" v-if="description">
       <p>{{description}}</p>
     </div>
-    <textarea class="initMessage" v-model="initMessage"></textarea>
+    <textarea class="initMessage" v-model="initMessage" placeholder="Enter your message here"></textarea>
     <button class="btn btn-submit" v-on:click="sendRequest(initMessage)">Ask now</button>
   </div>
   `;
