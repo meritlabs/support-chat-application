456d45ac628d900106de00e4595bf1de6f67b238
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-07 18:18:27 +0300

    Add base style for chat window

--- src/chat-form/components/chat/chat.view.ts
+++ src/chat-form/components/chat/chat.view.ts
@@ -1,8 +1,16 @@
 export default function template() {
   return `
   <div class="chatView">
-    <div v-for="item in messages">
-      {{item.message}}
+    <div class="messagesWindow">
+      <div v-for="item in messages">
+        {{item.message}}
+      </div>
+    </div>
+    <div class="inputBox">
+      <textarea placeholder="Enter your message..."></textarea>
+      <button>
+       Send
+      </button>
     </div>
   </div>
   `;
