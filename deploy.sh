d3292f4ac2c95d3f3ada3b043c00a7af7fd2e15e
Vladimir Gutorov <vlgutv@gmail.com>
2018-07-25 12:15:42 +0300

    update shell script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
-
-
 while true; do
-        read -p "Would you like to deploy to Merit Support BOT to production? [Y/N]: " yn
-        case $yn in
-            [Yy]* ) git checkout master && git pull && heroku git:remote -a merit-support-application && git push heroku master; break;;
-            [Nn]* ) heroku git:remote -a support-application && echo 'Enter current branch below' && read branch && git push heroku $branch:master; break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+  read -p "Would you like to deploy to Merit Support BOT to production? [Y/N]: " yn
+    case $yn in
+      [Yy]* ) git checkout master && git pull && heroku git:remote -a merit-support-application && git push heroku master; break;;
+      [Nn]* ) heroku git:remote -a support-application && echo 'Enter current branch below' && read branch && git push heroku $branch:master; break;;
+      * ) echo "Please answer yes or no.";;
+      esac
+done
